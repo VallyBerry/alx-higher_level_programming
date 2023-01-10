48a8d93a20354ad3b11264d189f1250f2e4257b8
vallyberry <andrekellie267@gmail.com>
2023-01-10 21:54:38 +0000

    corrections

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """prints the value of variable a from variable_load_5."""
-    from  variable_load_5 import a
 
+'''prints the value of variable a from variable_load_5.'''
+
+
+if __name__ == "__main__":
+    from  variable_load_5 import a
     print("{}".format(a))
